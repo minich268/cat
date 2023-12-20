a5571ffdbf410d0f6a639a1b50b4522bbc334646
Minich Lena <minich268@gmail.com>
2023-12-20 18:31:43 +0300

    ferst commit

---
+++ src/components/layout/HeaderComponent.vue
@@ -0,0 +1,78 @@
+<script lang="ts">/*eslint-disable*/ 
+import { Vue, Component } from "vue-property-decorator";
+@Component({})
+export default class HeaderComponent extends Vue {
+  menuIsActive: boolean = false;
+
+  menuNav() {
+    this.menuIsActive = !this.menuIsActive;
+  }
+}
+</script>
+<template>
+  <el-header height="auto">
+    <el-row type="flex" class="row-bg" justify="space-between">
+      <el-col class="header-logo" :span="12">
+        <a href="#">
+          <!--все картинки в папке assets*/ -->
+          <img src="@/assets/img/header-logo.svg" alt="logo" />
+        </a>
+      </el-col>
+      <el-col :span="12"
+        ><div>
+          <el-button plain size="mini" class="mobile-nav" @click="menuNav"
+            ><i v-if="menuIsActive" class="el-icon-menu"></i><i class="el-icon-close"></i
+          ></el-button>
+          <el-menu
+            :default-active="activeIndex"
+            mode="horizontal"
+            v-bind:class="{ '': !menuIsActive, ' isOpen': menuIsActive }"
+          >
+            <el-menu-item index="1"><router-link to="/">Home</router-link></el-menu-item>
+            
+            <el-menu-item index="3" disabled>Info</el-menu-item>
+            <el-menu-item index="4">
+              <router-link to="/about">About</router-link></el-menu-item
+            >
+          </el-menu>
+          <el-radio-group v-model="radio4" disabled size="mini">
+      <el-radio-button label="ru">RU</el-radio-button>
+      <el-radio-button label="en">Eng</el-radio-button>  
+      </el-radio-group>
+      <el-button size="mini" type="info"><i class="el-icon-upload el-icon-right"></i>Logout</el-button>
+        </div></el-col
+      >
+    </el-row>
+  </el-header>
+</template>
+<style lang="scss" scroped>
+.el-header {
+  padding: 0;
+  position: relative;
+}
+.el-menu--horizontal.el-menu {
+  border: none;
+  .el-menu-item {
+    background: transparent;
+    a {
+      font-size: 16px;
+      text-decoration: none;
+    }
+    &.is-active {
+      border: none;
+    }
+  }
+  @media (max-width: 768px) {
+    display: none;
+    opacity: 0;
+    background: #585757;
+  }
+  &.isOpen {
+    position: absolute;
+    width: 100%;
+    top: 60px;
+    display: block;
+    opacity: 1;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,49 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import LoginPageView from "../views/LoginPageView.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
+    path: "/login",
+    name: "login",
+    component: LoginPageView,
+  },
+  {
     path: "/",
     name: "home",
     component: HomeView,
+    meta: { requiresAuth: true },
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
+    meta: { requiresAuth: true },
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    const authUser = JSON.parse(
+      window.localStorage.getItem("currentUser") || "{}"
+    );
+    if (authUser && authUser.accessToken) {
+      next();
+    } else {
+      next({ name: "login" });
+    }
+  } else {
+    next(); // make sure to always call next()!
+  }
 });
 
 export default router;
